2558d802eaa1a0adfb20f1df5bdd95a7051ce85b
star_ranger01 <star_ranger01@comcast.net>
2024-03-09 21:48:04 -0800

    More starter files and random edits to older files to get the ball rolling.

---
+++ app_main.py
@@ -0,0 +1,18 @@
+# -*- coding: "utf-8" -*-
+
+# Import dependencies.
+import sys
+from frontend import st#, csv
+from frontend import csv_example as csv
+
+csv
+
+
+
+
+if __name__ == "__main__":
+    user_name = sys.argv[1]
+
+else:
+    print("Run as import")
+
---
+++ frontend/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: "utf-8" -*-
+import streamlit as st
+from pathlib import Path
+import pct_change as pct
