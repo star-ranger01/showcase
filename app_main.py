b3e1309183122b0424a58a37572cf466c9328f47
star_ranger01 <star_ranger01@comcast.net>
2024-03-10 17:14:25 -0700

    Redo by reset to initial. Files were lost and had to recreate them in this commit.

--- app_main.py
+++ app_main.py
@@ -5,7 +5,17 @@ import sys
 from frontend import st#, csv
 from frontend import csv_example as csv
 
-csv
+#csv
+#
+#with tab1:
+#    st.write("Wa na labaw")
+#option = st.sidebar.selectbox("Select data collection.", ("Pre-collected data in csv format", "Make an API request for data"))
+#
+#if option == "Pre-collected data in csv format":
+#    st.sidebar.write("My man.")
+#
+#else:
+#    st.sidebar.write("Dude.")
 
 
 
